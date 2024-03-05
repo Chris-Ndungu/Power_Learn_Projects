bccfb80016dc3e3bf7d3931fa199c3cf48a1ccf3
Chris Ndungu <128899227+Chris-Ndungu@users.noreply.github.com>
2024-03-05 10:35:14 +0300

    Initial Commit

---
+++ Python/user_input.py
@@ -0,0 +1,11 @@
+'''
+    a program that asks the user for their name, age, and location and then prints out a personalized message
+'''
+
+# get the user data
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
